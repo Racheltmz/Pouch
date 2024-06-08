3c6a344c48acc845587c6016c42b8775bcc91db4
snnjana <SHANMUGASUNDARAMSANJANA@GMAIL.COM>
2024-06-08 19:06:50 +0800

    updated homescreen

--- frontend/components/HomeScreen.js
+++ frontend/components/HomeScreen.js
@@ -1,23 +1,70 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Modal, Button, Image } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import QRCode from 'react-native-qrcode-svg';
-import StoresScreen from './StoresScreen';
 
 const Tab = createBottomTabNavigator();
 
-const HomeScreen = () => (
-  <View style={styles.container}>
-    <Text>Home Screen</Text>
-  </View>
-);
+const HomeScreen = ({navigation}) => {
+  const handleProfilePress = () => {
+    // Navigate to AccountScreen
+    navigation.navigate('Account');
+  };
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.profileContainer} onPress={handleProfilePress}>
+        <Image source={require('../assets/profile-icon.png')} style={styles.profileIcon} />
+        <Text style={styles.profileText}>Welcome{'\n'}Afreen!</Text>
+      </TouchableOpacity>
+      <View style={styles.pointsContainer}>
+          <Text style={styles.pointsContainerText}>Box Text</Text>
+        </View>
+      <Text>Home Screen</Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#fff'
+  },
+  profileContainer: {
+    position: 'absolute',
+    top: 0,
+    left: 5,
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10,
+  },
+  profileIcon: {
+    width: 50,
+    height: 50,
+    marginLeft: 10,
+    marginTop: 10,
+  },
+  profileText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginLeft: 17,
+    marginTop: 5,
+  },
+  pointsContainer: {
+    height: 30,
+    width: 60,
+    backgroundColor: '#88C34A',
+    borderRadius: 5,
+    position: 'absolute',
+    top: 30,
+    right: 80,
+    padding: 10,
+  },
+  pointsContainerText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#fff',
   },
 });
 
