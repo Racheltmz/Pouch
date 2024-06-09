bf514c6e7cc81255314688905fd577679abc0e1c
snnjana <SHANMUGASUNDARAMSANJANA@GMAIL.COM>
2024-06-09 15:48:36 +0800

    myrewards qr code screen ui

--- frontend/components/QRCodeScreen.js
+++ frontend/components/QRCodeScreen.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import QRCode from 'react-native-qrcode-svg';
 
-const QRCodeScreen = () => (
-  <View style={styles.container}>
-    <Text>QR Code Screen</Text>
-  </View>
-);
+const QRCodeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.qrTitle}>Voucher QR</Text>
+      <View style={styles.qrContainer}>
+        <QRCode value="https://www.example.com" size={270} />
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  qrTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    bottom: 100,
+  },
+  qrContainer: {
+    bottom: 90,
   },
 });
 
